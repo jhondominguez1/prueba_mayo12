e4c983ae58d3e1139c04045e5976c53ad4924079
jhondominguez1 <jjjdomi@gmail.com>
2016-05-12 11:32:45 -0500

    En este archivo lo voy a usar para logeo

---
+++ prueba3.php
@@ -0,0 +1,12 @@
+ El volumen de la unidad D es Particion_1
+ El n£mero de serie del volumen es: 0AAC-AFC3
+
+ Directorio de D:\local_prueba_mayo12
+
+12/05/2016  11:31 a. m.    <DIR>          .
+12/05/2016  11:31 a. m.    <DIR>          ..
+12/05/2016  10:29 a. m.               383 formulario.html
+12/05/2016  11:24 a. m.               438 prueba2.php
+12/05/2016  11:31 a. m.                 0 prueba3.php
+               3 archivos            821 bytes
+               2 dirs  76.885.925.888 bytes libres
