b0ccb70c0e01ac5038b758f1184b2ef5e893fae6
jhondominguez1 <jjjdomi@gmail.com>
2016-05-12 11:25:18 -0500

    En este archivo de prueba ingrese codigo php

---
+++ prueba2.php
@@ -0,0 +1,11 @@
+ El volumen de la unidad D es Particion_1
+ El n£mero de serie del volumen es: 0AAC-AFC3
+
+ Directorio de D:\local_prueba_mayo12
+
+12/05/2016  11:24 a. m.    <DIR>          .
+12/05/2016  11:24 a. m.    <DIR>          ..
+12/05/2016  10:29 a. m.               383 formulario.html
+12/05/2016  11:24 a. m.                 0 prueba2.php
+               2 archivos            383 bytes
+               2 dirs  76.885.934.080 bytes libres
